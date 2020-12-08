be22b55fcb0f6eb767bde05951487b27bffa495b
Razvan Gheghe <razvan.gheghe@ebs.msft>
2020-12-08 21:20:53 +0200

    test
    git push

--- app/controllers/car.js
+++ app/controllers/car.js
@@ -1,10 +1,12 @@
 const isAdmin = false;
 const car = require('../models/car');
 const Car = require('../models/car');
+// const { delete } = require('../routes/car');
 
 module.exports = {
     createCar: createCar,
     getCar: getCar,
+    deleteCar: deleteCar,
     responseToJSON : responseToJSON
 };
 
@@ -19,7 +21,7 @@ function createCar(req, res, next){
 
 function getCar(req, res, next){
     Car.find(function(err, result){
-        if(err){
+    if(err){
             return res.json(err)
         }     
         console.log(result);
@@ -29,6 +31,23 @@ function getCar(req, res, next){
     )
 }
 
+function deleteCar(req, res, next){
+    // const car = new Car(req.body);
+    // console.log(req.body._id);
+    car.deleteOne({_id: req.body._id}, function(err, result){
+        req.resources.car = result;
+        return next();
+    })
+}
+// function getUsersById(req, res, next){
+//     User.find({_id: '5fcf3aa2c640c42f28275d88'}, function(err, result){
+//         if(err){
+//             return res.json(err)
+//         }
+//         req.resources.users = result;
+//         return next();
+//     })
+// }
 function responseToJSON(prop){
     console.log("CAR", prop);
     return function(req, res, next) {
--- app/routes/car.js
+++ app/routes/car.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const initExpress  = require('../../config/express.js');
 const router = express.Router();
-const userController = require('../controllers/car.js')
+const userController = require('../controllers/car.js');
+const { isUserAdmin } = require('../controllers/users.js');
 
 router.post('/car',
     userController.createCar,
@@ -14,5 +15,11 @@ router.get('/car',
     userController.getCar,
     userController.responseToJSON('car')
 );
+
+router.delete('/car',
+    userController.deleteCar,
+    userController.responseToJSON('car')
+);
+
 
 module.exports = router;
